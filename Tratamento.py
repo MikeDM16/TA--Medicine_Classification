3d5101e2055aafe8881c712e47e1ffec5a0091d6
MikeDM16 <miguel_miranda96@live.com.pt>
2018-04-27 21:42:15 +0100

    Correção dataset

--- Tratamento.py
+++ Tratamento.py
@@ -13,76 +13,49 @@ class Tratamento():
 			
 		def load_dados(file_name, path_dref, path_dc):
 			# matriz com os dados do CSV groundTruthTable inicial
+			# M_data vai ter os 10000 linhas do ficheiro inicial 
 			M_data = self.read_csv_file(file_name)
 
 			# Aumentar os casos de treino com as imagens de referência
+			# adiciona 2 a 2, e assume que a próxima imagem será a 5000.jpg 
+			# M_data vai ficar com 12000 casos 
 			M_data = self.adicionar_imgs_ref(path_dref, path_dc, M_data)
 
 			# Matriz com o mapeamento entre o nome das cápsulas e um id da classe atribuido
 			ref_classes = self.create_ids_classes(path_dref)
 
+			# trocar os nomes grandalhoes por números de 0...1000, que representam as classes
 			M_data = self.troca_nomes_ids(ref_classes, M_data)
+
+			# descartar linha com nome colunas 
 			M_data = M_data[1:len(M_data)]
 
-			M_data = self.adicionar_imgs_DataAugmentation(path_dref, path_dc, M_data)
-			
+			# ATENÇÂO: Já criaste as imagens dataAugmentation DC
+			# Elas devem estar na pasta .\DataAugmentationDC
+			# self.Data_AugmentationDC(path_dref, path_dc, M_data)
+
+			# copiar as imagens criadas para a pasta dc, colocando no CSV e associanda a uma classe
+			# assume que a proxima imagem será a 7000
+			M_data = self.adicionar_imgs_DataAugmentationDC(path_dref, path_dc, M_data)
+
+			# ATENÇÂO: Infelizmente temo que voltar a criá-las 
+			# Criar imagens DRef dataaugmentation para uma pasta 
+			self.Data_AugmentationDRef(path_dref, path_dc, M_data)
+			# Atenção 2: temos que ir à definição da função colocar na variavel nome o tamanho da tua pasta dc atual
+			# copiar as imagens criadas para a pasta dc, colocando no CSV e associanda a uma classe
+			M_data = self.adicionar_imgs_DataAugmentationDRef(path_dref, path_dc, M_data)
+
 			return M_data
 
 		# Executar isto só 1 vez. Tendo as imagens preparadas não há necessidade de repetir isto
-		#M_data = load_dados(file_name, path_dref, path_dc)
-		M_data = self.read_csv_file("Dados_final.csv")
-		
-		#self.Data_Augmentation(path_dref, path_dc, M_data)
-		#M_data = self.adicionar_imgs_DataAugmentation(path_dref, path_dc, M_data)
+		M_data = load_dados(file_name, path_dref, path_dc)
+		#M_data = self.read_csv_file("Dados_final.csv")
 
 		return M_data
 	# -------------------------------------------------------------------------------------------
-	def adicionar_imgs_DataAugmentation(self, path_dref, path_dc, M_data):
-		#path_da = "./DataAugmentation/"
-		path_da = "./DataAugmentationDC/"
-		#da_imgs = os.listdir(path_da)
-		da_imgs = os.listdir(path_da)
-
-		nome = 7000
-
-		M_aux = [] 
-		
-		aux = np.zeros((1000))
-
-		i=0;
-		while(i < len(da_imgs)):
-			# abrir img em ./DataAugmentation
-			img = Image.open(path_da +  da_imgs[i])
-			# Copiar img para ./dc com novo nome 
-			new_file_name =  str(nome) + ".jpeg"
-			img.save(path_dc + new_file_name)
-
-			# O nome das imagens teve um bug. 
-			# Para saber a classe precisa desta conta
-			# é mais facil improvisar aqui que perder horas a criar novas imgs
-			classe = int(da_imgs[i].split("_")[0])
-			
-			aux[classe-1] = 1; 
-
-			# Adicionar os novos casos treino à matriz principal
-			M_data.append([str(classe), new_file_name])			
-			M_aux.append([str(classe), new_file_name])			
-			
-			i += 1
-			nome += 1
-
-			text = "Copying " + str(len(da_imgs)) + " Data augmentation images."
-			self.progress(i, len(da_imgs), text) 
-
-		self.save_csv(M_aux, "imgs_dataAugmentation.csv")
-		self.save_csv(M_data, "Dados_final.csv")
-		print(aux.sum())
-		print(i)
-		
-		return M_data
-
+	
 	# -------------------------------------------------------------------------------------------
-	# Cada imagem de referência (target) será adicionada ao folder DC 
+	# Cada imagem de referência (2000 target) será adicionada ao folder DC 
 	def adicionar_imgs_ref(self, path_dref, path_dc, M_data):
 		# Obter o nome de todas as imagens de referência. 
 		ref_imgs = os.listdir(path_dref)
@@ -206,8 +179,10 @@ class Tratamento():
 						falhou += 1
 						f.close()
 			print("Save " + file_name + " terminado. Falhou " + str(falhou) + " linhas.")
-	
 	# -------------------------------------------------------------------------------------------
+
+	# -------------------------------------------------------------------------------------------
+	# função para mostrar progress bar no loading de imagens 
 	def progress(self, count, total, status=''):
 		bar_len = 50
 		filled_len = int(round(bar_len * count / float(total)))
@@ -217,6 +192,8 @@ class Tratamento():
 
 		sys.stdout.write('[%s] %s%s    %s\r' % (bar, percents, '%', status))
 		sys.stdout.flush() 
+	# -------------------------------------------------------------------------------------------
+
 	# -------------------------------------------------------------------------------------------
 	def progress_pid(self, pid, procs, count, total, status=''):
 		bar_len = 50
@@ -229,7 +206,10 @@ class Tratamento():
 		with term.location(6, term.height - (procs+1-pid)):
 			print('[%s] %s%s    %s\r' % (bar, percents, '%', status), end='')
 	# -------------------------------------------------------------------------------------------
-	def data_augm_thread(self, path_dc, files, datagen, start, end, pid, procs):
+	
+	# -------------------------------------------------------------------------------------------
+	# -------------------------------------------------------------------------------------------
+	def data_augm_threadDC(self, path_dc, files, datagen, start, end, pid, procs):
 		save_to = "./DataAugmentationDC"
 		text = "From " + str(start) + " to " + str(end) + "."
 
@@ -246,20 +226,17 @@ class Tratamento():
 			
 			count = 0
 			for batch in datagen.flow(img, batch_size = 1, save_to_dir=save_to,
-							 		  save_prefix = str(classe), save_format = 'jpeg'):
+									  save_prefix = str(classe), save_format = 'jpeg'):
 				if count > 3: break
 				else: count += 1 
-
-	# -------------------------------------------------------------------------------------------
-	def Data_Augmentation(self, path_ref, path_dc, M_data):
+	
+	def Data_AugmentationDC(self, path_ref, path_dc, M_data):
 		start_time = time.time()
 
-		#files = os.listdir(path_ref)
 		files = M_data[0:10000]
 		feitos = np.zeros((5000,), dtype="uint8")
 		files_non_rep = []
-		# print("Files found at" + str(path_ref) + ": " + str(len(s)))
-		
+
 		pathlib.Path("DataAugmentationDC").mkdir(parents=True, exist_ok=True) 
 		save_to = "./DataAugmentationDC"
 
@@ -291,8 +268,129 @@ class Tratamento():
 			print("pid " + str(i))
 
 		results = Parallel(n_jobs=procs,backend="threading")\
-				  (delayed(self.data_augm_thread)(path_dc, files_non_rep, datagen, p*step_imgs, (p+1)*step_imgs, p, procs)
+				  (delayed(self.data_augm_threadDC)(path_dc, files_non_rep, datagen, p*step_imgs, (p+1)*step_imgs, p, procs)
 				  for p in range(0,procs))
 		
 		print("\n")
-		print("Tempo Data Augmentation: " + str(round((time.time() - start_time)/60, 2)) + " minutes")
+		print("Tempo Data Augmentation imagens treino (dc): " + str(round((time.time() - start_time)/60, 2)) + " minutes")
+	
+	# função que copia para ./dc as imagens criadas via dataAugmentation das imagens de treino (dc)
+	def adicionar_imgs_DataAugmentationDC(self, path_dref, path_dc, M_data):
+		path_da = "./DataAugmentationDC/"
+		da_imgs = os.listdir(path_da)
+
+		nome = 7000
+		M_aux = [] 		
+
+		i=0;
+		while(i < len(da_imgs)):
+			# abrir img em ./DataAugmentation
+			img = Image.open(path_da +  da_imgs[i])
+			# Copiar img para ./dc com novo nome 
+			new_file_name =  str(nome) + ".jpeg"
+			img.save(path_dc + new_file_name)
+
+			# saber a classe através no número inicial img
+			classe = int(da_imgs[i].split("_")[0])
+
+			# Adicionar os novos casos treino à matriz principal
+			M_data.append([str(classe), new_file_name])			
+			M_aux.append([str(classe), new_file_name])			
+			
+			i += 1
+			nome += 1
+
+			text = "Copying " + str(len(da_imgs)) + " Data augmentation images."
+			self.progress(i, len(da_imgs), text) 
+
+		self.save_csv(M_aux, "imgs_dataAugmentationDC.csv")
+		self.save_csv(M_data, "Dados_final.csv")
+		
+		return M_data
+	# -------------------------------------------------------------------------------------------
+	# -------------------------------------------------------------------------------------------
+
+	# -------------------------------------------------------------------------------------------
+	# -------------------------------------------------------------------------------------------
+	# Criar dataAugmentation com as 2000 imagens de treino da pasta dc 
+	def Data_AugmentationDRef(self, path_ref, path_dc, M_data):
+		start_time = time.time()
+
+		files = os.listdir(path_ref)
+
+		pathlib.Path("DataAugmentationDR").mkdir(parents=True, exist_ok=True) 
+		save_to = "./DataAugmentationDR"
+		
+		datagen = ImageDataGenerator(rotation_range=40,
+									 width_shift_range=0.2,
+									 height_shift_range=0.2,
+									 rescale=1./255,
+									 shear_range=0.2,
+									 zoom_range=0.2,
+									 horizontal_flip=True,
+									 fill_mode='nearest')
+		i = 0
+
+		nome_classes = self.read_csv_file("classes_ref-names.csv")
+		ref_names	= []
+		classes 	= []
+		ref_names	= [x[0] for x in nome_classes]
+		classes 	= [x[1] for x in nome_classes]
+
+		# retira repetidos
+		for f in files:
+			# Saber qual a classe atribuida à imagem de referencia a processar
+			nome_file = int(f.split(".")[0])
+			index = ref_names.index(nome_classes)
+			classe = classes[index]
+
+			# Load primeira imagem - Front 
+			img = load_img(path_dref + str(f)) # Keras function
+			img = img_to_array(img) # Keras function
+			img = img.reshape((1,) + img.shape) # Keras function
+			
+			count = 0
+			for batch in datagen.flow(img, batch_size = 1, save_to_dir=save_to,
+									  save_prefix = str(classe), save_format = 'jpeg'):
+				if count > 3: break
+				else: count += 1 
+			
+		print("Finished 6x Data augmentation of " + str(len(files)) + " reference images.")
+		print("Tempo Data Augmentation reference imgs: " + str(round((time.time() - start_time)/60, 2)) + " minutes")
+	# -------------------------------------------------------------------------------------------
+
+	# -------------------------------------------------------------------------------------------
+	# função que copia para ./dc as imagens criadas via dataAugmentation das imagens de referencia ./dr
+	def adicionar_imgs_DataAugmentationDRef(self, path_dref, path_dc, M_data):
+		path_da = "./DataAugmentationDR/"
+		dref_imgs = os.listdir(path_da)
+
+		''' Colocar aqui o tamanho da pasta dc ! '''
+		nome = (-1) 
+		M_aux = [] 		
+
+		i=0;
+		while(i < len(dref_imgs)):
+			# abrir img em ./DataAugmentation
+			img = Image.open(path_da +  dref_imgs[i])
+			# Copiar img para ./dc com novo nome 
+			new_file_name =  str(nome) + ".jpeg"
+			img.save(path_dc + new_file_name)
+
+			# saber a classe através no número inicial img
+			classe = int(dref_imgs[i].split("_")[0])
+
+			# Adicionar os novos casos treino à matriz principal
+			M_data.append([str(classe), new_file_name])			
+			M_aux.append([str(classe), new_file_name])			
+			
+			i += 1
+			nome += 1
+
+			text = "Copying " + str(len(dref_imgs), text) 
+
+		self.save_csv(M_aux, "imgs_dataAugmentationDRef.csv")
+		self.save_csv(M_data, "Dados_final.csv")
+		
+		return M_data
+	# -------------------------------------------------------------------------------------------
